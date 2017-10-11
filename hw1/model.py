51ce77960149a7f53b061c59b101395e48d4990b
raywu0123 <b05901189@ntu.edu.tw>
2017-10-12 00:02:41 +0800

    fix model

--- hw1/model.py
+++ hw1/model.py
@@ -3,6 +3,7 @@ from tensorflow.contrib import keras
 import random
 import numpy as np
 
+
 class RNN_model():
   def __init__(self, args):
     self.__dict__ = args.__dict__.copy()
@@ -13,10 +14,11 @@ class RNN_model():
         self._frames_holder = tf.placeholder(tf.float32, [None, self.window_size, self.dim])
         self._labels_holder = tf.placeholder(tf.float32, [None, self.n_class])
 
-        self._loss = self.calc_loss(self._frames_holder, self._labels_holder)
+        pred = self.get_pred(rnn_cells, self._frames_holder)
+        self._loss = self.calc_loss(pred, self._labels_holder)
         self._loss /= self.batch_size
+        self._acc = self.calc_acc(pred, self._labels_holder)
 
-        self._acc = self.calc_acc(self._frames_holder, self._labels_holder)
       self._eval = self.optimize(self._loss)
       _vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
       for _var in _vars:
@@ -25,7 +27,7 @@ class RNN_model():
     elif self.is_test():
       with tf.variable_scope('build'):
         pass
-    else: # validation
+    else:  # validation
       with tf.variable_scope('build'):
         pass
 
@@ -47,7 +49,7 @@ class RNN_model():
 
   def build_model(self, rnn_cells, frames, labels):
     pred = self.get_pred(rnn_cells, frames)
-    if  self.is_train():
+    if self.is_train():
       return self.calc_loss(pred, labels)
     elif self.is_test():
       return pred
@@ -55,38 +57,45 @@ class RNN_model():
       return None
 
   def initialize(self):
-    if self.rnn_type == 0:#LSTM
+    if self.rnn_type == 0:  # LSTM
       def unit_cell(fac):
         return tf.contrib.rnn.LSTMCell(fac, use_peepholes=True)
-    elif self.rnn_type == 1:#GRU
+    elif self.rnn_type == 1:  # GRU
       def unit_cell(fac):
         return tf.contrib.rnn.GRUCell(fac)
     rnn_cell = unit_cell
-    #dropout layer
+    # dropout layer
     if not self.is_test() and self.keep_prob < 1:
       def rnn_cell(fac):
         return tf.contrib.rnn.DropoutWrapper(unit_cell(fac),
                                              output_keep_prob=self.keep_prob)
+
     def rnn_cells(fac):
       return tf.contrib.rnn.MultiRNNCell([rnn_cell(fac)
                                           for _ in range(self.rnn_layer_num)])
+
     return rnn_cells
 
-  def is_train(self): return self.mode == 'train'
-  def is_valid(self): return self.mode == 'valid'
-  def is_test(self): return self.mode == 'test'
+  def is_train(self):
+    return self.mode == 'train'
+
+  def is_valid(self):
+    return self.mode == 'valid'
+
+  def is_test(self):
+    return self.mode == 'test'
 
   def rnn_output(self, inp, f_cells, b_cells, scope):
     seq_len = tf.constant(self.batch_size, dtype=tf.int64, shape=[self.batch_size])
     if self.use_bidirection:
       oup, st = tf.nn.bidirectional_dynamic_rnn(f_cells, b_cells, inp, seq_len,
-                                               dtype=tf.float32, scope=scope)
+                                                dtype=tf.float32, scope=scope)
       oup = tf.concat(oup, -1)
       st = tf.concat(st, -1)
     else:
-      oup, st =\
+      oup, st = \
         tf.nn.dynamic_rnn(f_cells, inp, seq_len, dtype=tf.float32, scope=scope)
-    oup =\
+    oup = \
       tf.reshape(oup, [self.batch_size, -1, self.fac * f_cells.state_size[0]])
     return oup, st[-1]
 
@@ -96,10 +105,11 @@ class RNN_model():
 
   def calc_acc(self, pred, labels):
     return tf.reduce_mean(tf.cast(tf.equal(tf.arg_max(labels, 1), tf.arg_max(pred, 1)), tf.float32))
+
   def optimize(self, loss):
     tvars = tf.trainable_variables()
     grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars),
-               self.max_grad_norm)
+                                      self.max_grad_norm)
     self.learning_rate = tf.train.exponential_decay(self.learning_rate,
                                                     self.step,
                                                     self.decay_steps,
@@ -108,16 +118,29 @@ class RNN_model():
     return opt.apply_gradients(zip(grads, tvars), global_step=self._step)
 
   @property
-  def pred(self): return self._pred
+  def pred(self):
+    return self._pred
+
   @property
-  def loss(self): return self._loss
+  def loss(self):
+    return self._loss
+
   @property
-  def loss(self): return self._acc
+  def acc(self):
+    return self._acc
+
   @property
-  def eval(self): return self._eval
+  def eval(self):
+    return self._eval
+
   @property
-  def step(self): return self._step
+  def step(self):
+    return self._step
+
   @property
-  def frames_holder(self): return self._frames_holder
+  def frames_holder(self):
+    return self._frames_holder
+
   @property
-  def labels_holder(self): return self._labels_holder
+  def labels_holder(self):
+    return self._labels_holder
