c35ee36a77d0af8597323ddbb8a8aac7c6d69a7d
raywu0123 <b05901189@ntu.edu.tw>
2017-10-13 01:27:56 +0800

    move model out

--- hw1/test.py
+++ hw1/test.py
@@ -31,12 +31,20 @@ def model(feed_frames):
   return pred
 
 args = config.parse_arguments()
+tf.reset_default_graph()
 
 
 feed_frames = tf.placeholder(tf.float32, [None, args.window_size, args.dim])
 feed_labels = tf.placeholder(tf.float32, [None, args.window_size])
 
-pred = model(feed_frames)
+cell = tf.contrib.rnn.GRUCell(num_units=args.hidden_size)
+init_state = cell.zero_state(args.batch_size, dtype=tf.float32)
+outputs, _ = tf.nn.dynamic_rnn(cell, feed_frames, initial_state=init_state, time_major=False)
+flatten_outputs = tf.reshape(outputs, [-1, args.hidden_size])
+dense1 = tf.layers.dense(flatten_outputs, 512, activation=tf.nn.relu)
+dense2 = tf.layers.dense(dense1, 512, activation=tf.nn.relu)
+dense3 = tf.layers.dense(dense2, 512, activation=tf.nn.relu)
+pred = tf.layers.dense(dense3, args.n_class)
 
 test_frames, _ = load_data('test')
 print(test_frames.shape)
