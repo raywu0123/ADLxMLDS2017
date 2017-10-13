4974274cdfdc85528a8a28c4b5dc7d0386a9a558
raywu0123 <b05901189@ntu.edu.tw>
2017-10-14 04:24:37 +0800

    revise test method

--- hw1/test.py
+++ hw1/test.py
@@ -36,7 +36,6 @@ with tf.Graph().as_default():
   with tf.name_scope('test'):
     test_args = copy.deepcopy(args)
     test_args.mode = 'test'
-    test_args.batch_size = args.window_size
   with tf.variable_scope('model', reuse=None):
     test_model = RNN_model(args=test_args)
 
@@ -48,21 +47,22 @@ with tf.Graph().as_default():
 
   test_frames, _ = load_data('test')
 
-
-  frame_scores = np.zeros([test_frames.shape[0], args.n_class], dtype=float)
   with sv.managed_session() as sess:
     # global_step = sess.run(test_model.step)
     # print('global step = {}'.format(global_step))
-
+    frame_scores = np.zeros([test_frames.shape[0], args.n_class], dtype=float)
     n_frames = test_frames.shape[0]
     for frame_id in tqdm(range(n_frames)):
-      batch_frames = np.zeros([args.window_size, args.window_size, args.dim])
-      for batch_idx, start_idx in enumerate(range(frame_id+1, frame_id-args.window_size+1, -1)):
-        batch_frames[batch_idx, :, :] = np.expand_dims(test_frames.take(range(start_idx, start_idx+args.window_size),
-                                                         mode='wrap', axis=0), 0)
+      batch_frames = np.zeros([args.batch_size, args.window_size, args.dim])
+      batch_frames[frame_id % args.window_size, :, :] = test_frames.take(
+        range(frame_id - args.window_size/2, frame_id + args.window_size/2),
+        mode='wrap', axis=0)
 
-      feed_dict = {test_model.frames_holder: batch_frames}
-      prediction = sess.run(test_model.pred, feed_dict=feed_dict)
-      frame_scores[frame_id] = prediction.trace()
+      if (frame_id + 1) % args.batch_size or frame_id == n_frames-1:
+        feed_dict = {test_model.frames_holder: batch_frames}
+        prediction = sess.run(test_model.pred, feed_dict=feed_dict)
+        frame_scores[frame_id-args.batch_size+1:frame_id+1] = prediction
 
-    frame_ints = frame_scores.argmax(axis=-1)
+    with open('./prediction.npy','wb+') as file:
+      np.save(file, frame_scores)
+      print('Prediction saved to {}') % format(file.name)
