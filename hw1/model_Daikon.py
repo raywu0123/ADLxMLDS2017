25f25559ef083b15dbdd88c979ff3895f3442794
raywu0123 <b05901189@ntu.edu.tw>
2017-10-14 22:38:34 +0800

    revise cnn

--- hw1/model_Daikon.py
+++ hw1/model_Daikon.py
@@ -148,5 +148,5 @@ class CNN_model(RNN_model):
     output, _ = self.rnn_output(reshape2, f1_cells, b1_cells, 'rnn_model')
 
     flatten_outputs = tf.reshape(output, [self.batch_size*self.window_size, -1])
-
+    dense = tf.layers.dense(flatten_outputs, self.n_class)
     return flatten_outputs
