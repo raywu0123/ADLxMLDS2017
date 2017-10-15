59f87948af8bb275bfa317f732ea8b2a4d2282c3
raywu0123 <b05901189@ntu.edu.tw>
2017-10-15 18:15:39 +0800

    revise testing

--- hw1/test.py
+++ hw1/test.py
@@ -128,7 +128,7 @@ with tf.Graph().as_default():
             += batch_pred
         else:
           frame_scores[frame_start_id:] += batch_pred[:n_frames-1-frame_end_id]
-          frame_scores[:frame_end_id-n_frames+1] += batch_pred[n_frames-frame_end_id:]
+          frame_scores[:frame_end_id-n_frames+1] += batch_pred[n_frames-1-frame_end_id:]
 
 
     write_result(frame_scores, args.pred_file)
